f4f56c0f55cec25fc143ac433989bdd03ee93b7c
Leandro <leandro.borges@petrobras.com.br>
2023-09-15 16:03:54 -0300

    initial version

---
+++ src/test/groovy/com/example/LogYamlSpec.groovy
@@ -0,0 +1,26 @@
+package com.example
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class LogYamlSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Inject
+    static final Logger logger = LoggerFactory.getLogger(LogYamlSpec.class)
+
+    void 'test it works'() {
+        logger.info('log yaml spec')
+
+        expect:
+        application.running
+    }
+
+}
